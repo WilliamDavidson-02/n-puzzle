8cf98a724bdacd18698f096fc0203cf2627f48bb
William Davidson <wj.davidson@outlook.com>
2024-05-17 10:47:16 +0200

    initial files

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Button from "@components/Button";
+
+const App = () => {
+  return (
+    <main>
+      <h1>Puzzle</h1>
+      <Button>Click me</Button>
+    </main>
+  );
+};
+
+export default App;
